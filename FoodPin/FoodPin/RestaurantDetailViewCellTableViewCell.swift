b0ee9b479270dde95c4c2d45eb81997e0a3e649d
Luis Lira <luislira1029@gmail.com>
2019-01-12 01:40:23 -0600

    Applications development

---
+++ FoodPin/FoodPin/RestaurantDetailViewCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantDetailViewCellTableViewCell.swift
+//  FoodPin
+//
+//  Created by LABMAC11 on 14/03/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
